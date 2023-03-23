331045184d60ec0f324d2930d775cfe378948bb2
Dan Olson <danolson100@gmail.com>
2023-03-22 19:09:46 -0500

    Update fzf.bash to not quote paths in sources

--- fzf.bash
+++ fzf.bash
@@ -6,8 +6,8 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "~/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source ~/.fzf/shell/completion.bash 2> /dev/null
 
 # Key bindings
 # ------------
-source "~/.fzf/shell/key-bindings.bash"
+source ~/.fzf/shell/key-bindings.bash
